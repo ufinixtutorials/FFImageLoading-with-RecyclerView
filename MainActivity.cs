4e670f2cdcd2dc5d055da014db2ec519b891a4f2
elatech.limited <elatech.limited@outlook.com>
2019-10-31 18:09:49 +0100

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Support.V7.Widget;
+using System.Collections.Generic;
+
+namespace ImageLoading
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        RecyclerView recyclerView;
+        List<string> ImageUrlList = new List<string>();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            recyclerView = (RecyclerView)FindViewById(Resource.Id.recyclerView);
+             CreateData();
+             SetupRecyclerView();
+        }
+
+        void CreateData()
+        {
+            for (int i = 1; i < 21; i++)
+            {
+                string url = $"https://picsum.photos/id/{i}/2560/1600";
+                ImageUrlList.Add(url);
+            }
+        }
+
+
+        void SetupRecyclerView()
+        {
+            recyclerView.SetLayoutManager(new Android.Support.V7.Widget.LinearLayoutManager(recyclerView.Context));
+            PostAdapter postAdapter = new PostAdapter(ImageUrlList);
+            recyclerView.SetAdapter(postAdapter);
+        }
+
+    }
+}
---
+++ PostAdapter.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.Widget;
+using System.Net;
+using System.IO;
+using Android.Graphics;
+using System.Collections.Generic;
+using FFImageLoading;
+
+namespace ImageLoading
+{
+    public class PostAdapter : RecyclerView.Adapter
+    {
+        public event EventHandler<PostAdapterClickEventArgs> ItemClick;
+        public event EventHandler<PostAdapterClickEventArgs> ItemLongClick;
+        List<string> items;
+
+        public PostAdapter(List<string> data)
+        {
+            items = data;
+        }
+
+        // Create new views (invoked by the layout manager)
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+
+            //Setup your layout here
+            View itemView = null;
+            var id = Resource.Layout.row;
+            itemView = LayoutInflater.From(parent.Context).Inflate(id, parent, false);
+
+            var vh = new PostAdapterViewHolder(itemView, OnClick, OnLongClick);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        public override void OnBindViewHolder(RecyclerView.ViewHolder viewHolder, int position)
+        {
+            var item = items[position];
+
+            // Replace the contents of the view with that element
+            var holder = viewHolder as PostAdapterViewHolder;
+          // GetImageNormal(item.ToString(), holder.postImage);
+
+            GetImage(item.ToString(), holder.postImage);
+        }
+
+        void GetImage(string url, ImageView imageView)
+        {
+            ImageService.Instance.LoadUrl(url)
+                .Retry(3, 200)
+                .DownSample(400, 400)
+                .Into(imageView);
+        }
+
+
+        async void GetImageNormal(string imageUrl, ImageView imageView)
+        {
+            // Download Image using WebRequest
+            System.Net.WebRequest request = default(System.Net.WebRequest);
+            request = WebRequest.Create(imageUrl);
+            request.Timeout = int.MaxValue;
+            request.Method = "GET";
+
+            WebResponse response = default(WebResponse);
+            response = await request.GetResponseAsync();
+            MemoryStream ms = new MemoryStream();
+            response.GetResponseStream().CopyTo(ms);
+            byte[] imageData = ms.ToArray();
+
+            Bitmap bitmap = BitmapFactory.DecodeByteArray(imageData, 0, imageData.Length);
+            imageView.SetImageBitmap(bitmap);
+
+        }
+
+      
+        public override int ItemCount => items.Count;
+
+        void OnClick(PostAdapterClickEventArgs args) => ItemClick?.Invoke(this, args);
+        void OnLongClick(PostAdapterClickEventArgs args) => ItemLongClick?.Invoke(this, args);
+
+    }
+
+
+
+    public class PostAdapterViewHolder : RecyclerView.ViewHolder
+    {
+        public ImageView postImage { get; set; }
+
+
+        public PostAdapterViewHolder(View itemView, Action<PostAdapterClickEventArgs> clickListener,
+                            Action<PostAdapterClickEventArgs> longClickListener) : base(itemView)
+        {
+            postImage = (ImageView)itemView.FindViewById(Resource.Id.postImage);
+            itemView.Click += (sender, e) => clickListener(new PostAdapterClickEventArgs { View = itemView, Position = AdapterPosition });
+            itemView.LongClick += (sender, e) => longClickListener(new PostAdapterClickEventArgs { View = itemView, Position = AdapterPosition });
+        }
+    }
+
+    public class PostAdapterClickEventArgs : EventArgs
+    {
+        public View View { get; set; }
+        public int Position { get; set; }
+    }
+}
